9610a744d6c79788e0211a651181a95cf9f13641
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2018-05-21 11:50:29 -0300

    Get DPI of images from a folder.

---
+++ Get_DPI_Of_images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script para verificar o DPI da imagem  
+# (Colocar no diretório com as imagens, dar a permissão 777 via chmod e executar
+# com ./script.sh   só e somente.... (Shell script é TOP).
+
+diretorio=$(ls *.jpg)
+
+for entrada in $diretorio;
+do
+    #echo $entrada
+    echo $entrada ' -  ' $(identify -verbose $entrada | grep Resolution) >> output.txt
+done
