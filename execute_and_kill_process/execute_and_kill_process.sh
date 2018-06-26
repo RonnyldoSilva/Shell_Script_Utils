023d8ed546f615a3e6f18e2affe1f076cb8a39ad
ronnyldo <ronnyldo.silva@ccc.ufcg.edu.br>
2018-06-26 04:34:23 -0300

    Now it can be save the id of a program, inside the id.txt.

--- execute_and_kill_process/execute_and_kill_process.sh
+++ execute_and_kill_process/execute_and_kill_process.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sh example.sh & echo $!
+# Run the first argument and save its id in a file
+sh $1 & echo $! > id.txt
 
+# Wait a little bit just to test this algorithm
 sleep 20
-kill $!
+
+# Read id of program
+line=$(head -n 1 id.txt)
+
+# Finish the program with its id
+kill $line
